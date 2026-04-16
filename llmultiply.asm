2c4d69f0a7ce09b021d2218a773016864403953a
Noel <noegi438@student.liu.se>
2026-04-16 12:46:47 +0200

    Initial commit

---
+++ llmultiply.asm
@@ -0,0 +1,125 @@
+;
+;  Test cases. All numbers are in hex.
+;
+;      111122223333 *     555566667777 =          5B061D958BF0ECA7C0481B5
+;  3456FEDCAAAA1000 * EDBA00112233FF01 = 309A912AF7188C57E62072DD409A1000
+;  FFFFEEEEDDDDCCCC * BBBBAAAA99998888 = BBBB9E2692C5DDDCC28F7531048D2C60
+;  FFFFFFFFFFFFFFFF * FFFFFFFFFFFFFFFF = FFFFFFFFFFFFFFFE0000000000000001
+;  00000001FFFFFFFF * 00000001FFFFFFFF = 0000000000000003FFFFFFFC00000001
+;  FFFEFFFFFFFFFFFF * FFFF0001FFFFFFFF = FFFE0002FFFDFFFE0001FFFE00000001
+;
+
+;  void llmultiply(unsigned long long int l1,
+;                  unsigned long long int l2,
+;                  unsigned char *result);
+
+        
+	SECTION .data
+
+
+	SECTION .text
+	ALIGN	16
+	BITS	32
+	
+	;; To access parameter BH on the stack you do
+	;;   MOV EAX, [EBP + 20]
+	;; Replace the zero below with the correct value to be able
+	;; to write your assembly code a little bit more readable: 
+	;;   MOV EAX, [EBP + BH_OFF]
+AL_OFF	EQU     8	; Offset from EBP to low  bits of a (AL)
+AH_OFF	EQU     12	; Offset from EBP to high bits of a (AH)
+BL_OFF	EQU     16	; Offset from EBP to low  bits of b (BL)
+BH_OFF	EQU     20	; Offset from EBP to high bits of b (BH)
+RES_OFF	EQU     24	; Offset from EBP to result array pointer
+	;;    ^^^^^ Replace 0 with correct values above
+        
+	GLOBAL llmultiply
+
+llmultiply:
+	PUSH EBP
+	MOV EBP, ESP
+
+	PUSH EAX
+	PUSH EBX
+	PUSH ECX
+	PUSH EDX
+	PUSH ESI
+	PUSH EDI
+	XOR ECX,ECX
+	
+
+
+	;; hämtar ut parameter ut stack
+	MOV EAX,[EBP+AL_OFF]
+	MOV EDX,[EBP+BL_OFF]
+	MUl EDX
+
+
+	
+	;; hämta resultat arrays address
+	mov EBX,[EBP+RES_OFF]
+	
+	;; ladda in resultat (AL*BL)L i resultat array
+	MOV [EBX],EAX
+
+	 MOV ESI, EDX
+
+
+	
+	;; hämtar ut parameter ut stack
+	MOV EAX, [EBP+ AH_OFF]
+	MOV EDX,[EBP+ BL_OFF]
+	MUL EDX  
+
+	ADD ESI, EAX 
+	ADC ECX, 0	;ECX används alltid till carry
+	MOV EDI,EDX
+
+	
+	MOV EAX, [EBP+ AL_OFF]
+	MOV EDX, [EBP+ BH_OFF]
+	MUL EDX
+
+	ADD EAX, ESI 
+	ADC ECX, 0	
+
+	;; ladda in resultat i resultat array
+	MOV [EBX+4], EAX
+	MOV ESI, EDX
+
+	
+;; second 32
+
+	
+	ADD ESI,ECX 
+	XOR ECX,ECX
+	ADC ECX,0
+
+	ADD EDI, ESI
+	ADC ECX, 0	  
+	mov ESI, EDI 
+
+	MOV EAX, [EBP+ AH_OFF]
+	MOV EDX,[EBP+ BH_OFF]
+	MUL EDX 
+
+	ADD EAX, ESI
+	ADC ECX, 0
+	MOV [EBX+8], EAX
+
+	 ;;third done
+
+	
+	ADD EDX,ECX 
+	MOV [EBX+12], EDX 
+
+	POP EDI
+	POP ESI
+	POP EDX
+	POP ECX
+	POP EBX
+	pop EAX
+	
+	POP EBP				; restore EBP reg
+	RET				;  return
+
---
+++ main.c
@@ -0,0 +1,103 @@
+#include <libepc.h>
+
+//void llllmultiply(unsigned long long int l1,unsigned long long int l2, unsigned char *result);
+
+struct test_case {
+  unsigned long long int a;
+  unsigned long long int b;
+  unsigned long long int rh;
+  unsigned long long int rl;
+};
+
+struct test_case cases[6] = {
+
+  { 0x0000111122223333ULL, 0x0000555566667777ULL,
+    0x0000000005B061D9ULL, 0x58BF0ECA7C0481B5ULL },
+
+  { 0x3456FEDCAAAA1000ULL, 0xEDBA00112233FF01ULL,
+    0x309A912AF7188C57ULL, 0xE62072DD409A1000ULL },
+
+  { 0xFFFFEEEEDDDDCCCCULL, 0xBBBBAAAA99998888ULL,
+    0xBBBB9E2692C5DDDCULL, 0xC28F7531048D2C60ULL },
+
+  { 0xFFFFFFFFFFFFFFFFULL, 0xFFFFFFFFFFFFFFFFULL,
+    0xFFFFFFFFFFFFFFFEULL, 0x0000000000000001ULL },
+  
+  { 0x00000001FFFFFFFFULL, 0x00000001FFFFFFFFULL,
+    0x0000000000000003ULL, 0xFFFFFFFC00000001ULL },
+
+  { 0xFFFEFFFFFFFFFFFFULL, 0xFFFF0001FFFFFFFFULL,
+    0xFFFE0002FFFDFFFEULL, 0x0001FFFE00000001ULL }
+};
+
+void PutUnsignedLongLong(unsigned long long int* ulli)
+{
+  unsigned long int* uli = (unsigned long int*)ulli; 
+  PutUnsigned(uli[1], 16, 8);
+  PutUnsigned(uli[0], 16, 8); 
+}
+
+
+
+
+void llmultiply(unsigned long long int a, unsigned long long int b, unsigned char *result){
+  
+  unsigned long int ALow=((a<<32)>>32);
+  unsigned long int AHigh=(a>>32);
+  unsigned long int BLow=((b<<32)>>32);
+  unsigned long int BHigh=(b>>32);		     
+  ///-------------///
+
+  unsigned long long int AL_BL=ALow*BLow;
+  unsigned long long int AL_BH=ALow*BHigh;
+  unsigned long long int AH_BL=AHigh*BLow;
+  unsigned long long int AH_BH=AHigh*BHigh;
+
+  //--------------///
+  unsigned char *resultPtr=result;
+  
+   
+
+  unsigned long int AL_BLLOW=(AL_BL & 0x00000000FFFFFFFF);
+  result[0]=AL_BLLOW;
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+  unsigned char result[16];
+  int i;
+  
+  ClearScreen(0x07);
+  SetCursorPosition(0, 0);
+
+  for (i = 0; i < 6; ++i)
+    {
+      PutString("Test : ");
+      PutUnsignedLongLong(&cases[i].a);
+      PutString(" * ");
+      PutUnsignedLongLong(&cases[i].b); 
+      PutString("\r\n");
+   
+      PutString("    == ");
+      PutUnsignedLongLong(&cases[i].rh);
+      PutUnsignedLongLong(&cases[i].rl);
+      PutString("\r\n");
+    
+      llmultiply(cases[i].a, cases[i].b, result);
+    
+      PutString("Result ");
+      PutUnsignedLongLong((unsigned long long int*)&result[8]);
+      PutUnsignedLongLong((unsigned long long int*)&result[0]); 
+      PutString("\r\n");
+    
+      PutString("\r\n");
+    }
+
+  return 0;
+}
+
+
+
