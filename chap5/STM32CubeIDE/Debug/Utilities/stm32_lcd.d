afbb3e0a1e1712b2ca17c520df3c28c41beefbe1
Anton <P.antons@proton.me>
2026-05-06 17:16:30 +0200

    labb 5 skellett

---
+++ chap5/STM32CubeIDE/Debug/Utilities/stm32_lcd.d
@@ -0,0 +1,20 @@
+Utilities/stm32_lcd.o: \
+ /home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/stm32_lcd.c \
+ /home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/stm32_lcd.h \
+ /home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/fonts.h \
+ ../../Drivers/TicTacToe/Components/Common/lcd.h \
+ /home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/font24.c \
+ /home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/fonts.h \
+ /home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/font20.c \
+ /home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/font16.c \
+ /home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/font12.c \
+ /home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/font8.c
+/home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/stm32_lcd.h:
+/home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/fonts.h:
+../../Drivers/TicTacToe/Components/Common/lcd.h:
+/home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/font24.c:
+/home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/fonts.h:
+/home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/font20.c:
+/home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/font16.c:
+/home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/font12.c:
+/home/rahmed/STM32CubeIDE/workspace_1.14.0/TicTacToe/Utilities/lcd/../Fonts/font8.c:
